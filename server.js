a80d099b9c7e6d7f440f0f18dbc78797a8c3100c
shm <malekishayan79@gmail.com>
2023-01-02 13:37:02 +0330

    feat(db): connect to mongo

--- server.js
+++ server.js
@@ -1,4 +1,11 @@
 // connet to db
+const config = require("config");
+const mongoose = require("mongoose");
+
+console.log(config.get("db"));
+
+mongoose.connect(config.get("db"));
+
 // get app
 const app = require("./src");
 
