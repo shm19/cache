59ff101550adf2c5b2caafc8122f6e2dc2e02130
shm <malekishayan79@gmail.com>
2023-01-02 23:08:53 +0330

    feat(morgan): add morgan logger

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
 import express from 'express';
-
+import morgan from 'morgan';
 import apiRouter from './routes/apiRotues';
+
 const app = express();
+
+// check if ENV is development
+app.use(morgan('dev'));
 
 app.use(express.json());
 
