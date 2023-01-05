9e915e22434092496ba9a2974b3d46c0a50b0b8d
shm <malekishayan79@gmail.com>
2023-01-05 22:14:30 +0330

    feat(errror): add AppError class

---
+++ src/utils/AppError.ts
@@ -0,0 +1,25 @@
+import { NextFunction, Request, Response } from 'express';
+
+export class AppError extends Error {
+  readonly statusCode: number;
+  readonly status: string;
+  readonly isOperational: boolean;
+  readonly type: string;
+
+  constructor(message: string, type: string, statusCode: number) {
+    super(message);
+    this.statusCode = statusCode;
+    this.status = `${statusCode}`.startsWith('4') ? 'fail' : 'error';
+    this.isOperational = true;
+    this.type = type;
+    Error.captureStackTrace(this, this.constructor);
+  }
+}
+
+export const catchAsync =
+  (fn: Function) => (req: Request, res: Response, next: NextFunction) =>
+    fn(req, res, next).catch(next);
+
+export enum CommonErrors {
+  JOI_VALIDATION = 400,
+}
