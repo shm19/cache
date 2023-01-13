8a34494ee01b58ccff3cd8bb4da21010f29f116b
shm <malekishayan79@gmail.com>
2023-01-14 01:34:40 +0330

    feat(error): log error in globalErrorHandler

--- src/controller/errorController.ts
+++ src/controller/errorController.ts
@@ -3,15 +3,17 @@ import { AppError } from '../utils/AppError';
 
 export const globalErrorHandler = (
   err: AppError,
-  _req: Request,
+  req: Request,
   res: Response,
   _next: NextFunction
 ) => {
   if (process.env.NODE_ENV === 'development') {
+    req.logger.error(err);
     res.status(err.statusCode).json({ err });
   } else if (process.env.NODE_ENV === 'production') {
+    req.logger.error(err);
     res
       .status(err.statusCode)
-      .json({ status: err.status, message: err.message, type: err.type });
+      .json({ status: err.status, message: err.message, type: err.errType });
   }
 };
--- src/middleware/validate.ts
+++ src/middleware/validate.ts
@@ -6,12 +6,13 @@ export const joiValidate =
   (validator: Function) =>
   (req: Request, _res: Response, next: NextFunction) => {
     const { error } = validator(req.body);
-    next(
-      new AppError(
-        error.details[0].message,
-        CommonErrors[CommonErrors.JOI_VALIDATION],
-        HttpStatusCode.BAD_REQUEST
-      )
-    );
+    if (error)
+      return next(
+        new AppError(
+          error.details[0].message,
+          CommonErrors[CommonErrors.JOI_VALIDATION],
+          HttpStatusCode.BAD_REQUEST
+        )
+      );
     next();
   };
