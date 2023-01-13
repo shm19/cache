bc96ce0a5581e977487dbd32599e94c00d65f1bb
shm <malekishayan79@gmail.com>
2023-01-14 02:23:13 +0330

    fix(error): bugs comes from test

--- src/controller/errorController.ts
+++ src/controller/errorController.ts
@@ -2,18 +2,24 @@ import { Request, Response, NextFunction, ErrorRequestHandler } from 'express';
 import { AppError } from '../utils/AppError';
 
 export const globalErrorHandler = (
-  err: AppError,
+  error: AppError,
   req: Request,
   res: Response,
   _next: NextFunction
 ) => {
+  const errorObj = { ...error, message: error.message };
   if (process.env.NODE_ENV === 'development') {
-    req.logger.error(err);
-    res.status(err.statusCode).json({ err });
+    req.logger.error(errorObj);
+    res.status(error.statusCode).json(errorObj);
   } else if (process.env.NODE_ENV === 'production') {
-    req.logger.error(err);
-    res
-      .status(err.statusCode)
-      .json({ status: err.status, message: err.message, type: err.errType });
+    req.logger.error(error);
+    res.status(error.statusCode).json({
+      status: error.status,
+      message: error.message,
+      type: error.errorType,
+    });
+  } else {
+    req.logger.info(errorObj);
+    res.status(error.statusCode).json(errorObj);
   }
 };
--- src/utils/AppError.ts
+++ src/utils/AppError.ts
@@ -4,14 +4,14 @@ export class AppError extends Error {
   readonly statusCode: number;
   readonly status: string;
   readonly isOperational: boolean;
-  readonly errType: string;
+  readonly errorType: string;
 
-  constructor(message: string, errType: string, statusCode: number) {
+  constructor(message: string, errorType: string, statusCode: number) {
     super(message);
     this.statusCode = statusCode;
     this.status = `${statusCode}`.startsWith('4') ? 'fail' : 'error';
     this.isOperational = true;
-    this.errType = errType;
+    this.errorType = errorType;
     Error.captureStackTrace(this, this.constructor);
   }
 }
