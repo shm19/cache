ed6e91f48401458a501994642b8b0cb0c011ac98
shm <malekishayan79@gmail.com>
2023-01-14 02:21:00 +0330

    feat(config): read port from default config

--- server.ts
+++ server.ts
@@ -6,6 +6,6 @@ import app from './src';
 mongoose.set('strictQuery', false);
 mongoose.connect(config.get('db'));
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log('Server is running...');
+app.listen(config.get('port') || 3000, () => {
+  console.log(`Server is running... ${config.get('port')}`);
 });
